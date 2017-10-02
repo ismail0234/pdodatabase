5f727c432f651384fad8bc386605c8d396646eba
İsmail <ismaiil_0234@hotmail.com>
2017-10-03 00:31:15 +0300

    pdo database recreate

--- application/database/database.php
+++ application/database/database.php
@@ -5,39 +5,22 @@ Class PDO_MYSQL
 
      protected $debugcss = '<style type="text/css">::selection{background-color:#E13300;color:#fff}::-moz-selection{background-color:#E13300;color:#fff}body{background-color:#fff;margin:40px;font:13px/20px normal Helvetica,Arial,sans-serif;color:#4F5155}a{color:#039;background-color:transparent;font-weight:400}h1{color:#444;background-color:transparent;border-bottom:1px solid #D0D0D0;font-size:19px;font-weight:400;margin:0 0 14px;padding:14px 15px 10px}code{font-family:Consolas,Monaco,Courier New,Courier,monospace;font-size:12px;background-color:#f9f9f9;border:1px solid #D0D0D0;color:#002166;display:block;margin:14px 0;padding:12px 10px}#container{margin:10px;border:1px solid #D0D0D0;box-shadow:0 0 8px #D0D0D0}p{margin:12px 15px}</style>';
 
-
-     protected $selectarr = array();
-     
-     protected $whereand = array();
-
-     protected $whereor = array();
-
-     protected $orderby = array();
-
-	 protected $from = null;
-
-     protected $where = array("var" => [],"sql" => "");
-
-     protected $limit = array();
-
      protected $prefix = '';
 
      private $pdo = null;
 
-     public function __construct(){
-		 global $fw_config;
+     public function __construct($array = [])
+     {
 
-         $connstring = $fw_config["database"]["dbengine"].":host=".$fw_config["database"]["ip"].";dbname=".$fw_config["database"]["database"].";charset=".$fw_config["database"]["charset"];
+         $connstring = $array["dbengine"].":host=".$array["ip"].";dbname=".$array["database"].";charset=".$array["charset"];
 
 		 try{
 
 
-			 $this->pdo = new PDO($connstring,$fw_config["database"]["username"],$fw_config["database"]["password"]);
+			 $this->pdo = new PDO($connstring,$array["username"],$array["password"]);
 
 			 $this->pdo->setAttribute( PDO::ATTR_EMULATE_PREPARES, false );
-//             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-			 $this->prefix = $fw_config["database"]["prefix"];
+			 $this->prefix = $array["prefix"];
 
 		 }catch(Exception $e){
 
@@ -46,135 +29,9 @@ Class PDO_MYSQL
 		 }
 
 	 }
-     
 
-     public function select($s = '*'){
-
-          if(is_string($s)){
-
-          		$sarr = explode(',',$s);
-
-          }
-          
-          foreach($sarr as $sar){
-
-               $this->selectarr[] = $sar;
-
-          }
-
-          return $this;
-     }
-
-     public function from($fr){
-
- 		 $this->from = $fr;
-         
-         return $this;
-
-     }
-
-     public function or_where($arr = []){
-        
-        return $this->where_in_where($arr,'OR');
-
-     }
-
-     public function where($arr = []){
-        
-        return $this->where_in_where($arr,'AND');
- 		 
-     }
-
-
-     private function where_in_where($arr,$ara = 'AND',$isaret = '='){
-      
- 		  $ara = trim($ara);
-
-	      switch ($ara) {
-	      	case 'OR':
-			         foreach((array)$arr as $key => $value){
-			     		$this->whereor[$key]["val"] = $value;
-			     		$this->whereor[$key]["tur"] = $isaret;
-			         }
-	      		break;
-	      	
-	      	default:
- 			         foreach((array)$arr as $key => $value){
-			     		$this->whereand[$key]["val"] = $value;
-			     		$this->whereand[$key]["tur"] = $isaret;
-
-			         }
-      		break;
-	      }
-        
-        return $this;
-
-     }
-     
-     private function where_combine($arr,$isaret = 'AND'){
-
-    	$sql = [];
-
-
-        if(!empty($this->where["sql"])){
-
-	        $sql[] = $this->where["sql"];
-
-        }
-        if(count($arr) >  0){
-            foreach((array)$arr as $key => $value){
-                
-                if(empty($key)){
-
-                    continue;
-                }
-
-                $sql[] = $key.' '.$value["tur"].' ?';
-                $this->where["var"][] = $value["val"];
-
-            }  
-        }
-  	
-
-        if(count($sql) > 0){
-
-            $this->where["sql"] = implode(' '.$isaret.' ', $sql);
-
-        }
-
-     }
-
-     public function limit($min,$max = 0){
-
-        $this->limit = array($min,$max);
-   
-   		return $this;
-     }
-     
-     private function limit_combine($arr){
-
-        $sql = '';
-        $var = [];
-
-        if(isset($arr[1]) && $arr[1] > 0){
-
-            $sql = 'LIMIT ?,?';
-            $var = $arr;
-
-        }else if(count($arr) > 0){
-
-            $sql = 'LIMIT ?';
-            $var = array($arr[0]);
-
-
-        }
-
-        return array('sql' => $sql, 'var' => $var); 
-
-
-     }
-
-     private function debug($name,$arr = [],$sql){
+     private function debug($name,$arr = [],$sql)
+     {
 
        echo $this->debugcss;
 
@@ -188,26 +45,6 @@ Class PDO_MYSQL
      }
 
 
-     public function insert($table = '',$arr = []){
-
-     	 $sql = [];
-
-     	 foreach((array)$arr as $key => $value){
-
-     	 	$sql[0][] = htmlspecialchars($key);
-     	 	$sql[1][] = '?';
-     	 	$sql[2][] = htmlspecialchars($value);
-
-     	 }
-
-         $sqlstr = "INSERT INTO ".$this->prefix.$table." (".implode(',',$sql[0]).") VALUES(".implode(',',$sql[1]).")";
-
-     	 $pre = $this->pdo->prepare($sqlstr);
-
-         $this->pdoexec($sqlstr,$sql[2]);
-
-
-     }
      public function insert_batch($table = '',$arr = []){
 
      	 $sql = [];
@@ -233,20 +70,36 @@ Class PDO_MYSQL
 
          $this->pdoexec($sqlstr,$sql[2]);
      }
+     public function check($table = [])
+     {
 
+        $tablename = $this->array_get($table);
 
-     private function pdoexec($sql,$array,$status = 0){
+        if(empty($tablename))
+        {
+
+            return false;
+
+        }
+
+        return $this->pdoexec('CHECK TABLE ' . $tablename,[],6);
+
+     }
+
+     private function pdoexec($sql,$array = [],$status = 0){
 
          $pre = $this->pdo->prepare($sql);
 
          $errorCode = $this->pdo->errorInfo();
+
          if($errorCode[0] > 0){
 
             $this->debug('SQL Prepare Error',$errorCode,$sql);
             return false;
          }
 
-         if(!$pre->execute($array)){
+         if(!$pre->execute($array))
+         {
 
             $this->debug('SQL Execute Error',$pre->errorInfo(),$sql);
             return false;
@@ -254,7 +107,8 @@ Class PDO_MYSQL
 
          $sonuc = true;
 
-         switch($status){
+         switch($status)
+         {
 
             case 1: $sonuc = $pre->fetchAll(PDO::FETCH_OBJ);  break;
             case 2: $sonuc = $pre->fetchAll();  break;
@@ -265,138 +119,166 @@ Class PDO_MYSQL
 
          }
 
-         $this->clearQuery();
-
          return $sonuc;
 
      }
 
-     public function truncate($table){
 
-           $this->pdoexec('TRUNCATE '.htmlspecialchars($table).'',array());
+
+
+
+
+
+
+
+     public function insert($table = '',$arr = [])
+     {
+
+         $sql = [];
+
+         foreach((array)$arr as $key => $value){
+
+            $sql[0][] = trim($key);
+            $sql[1][] = '?';
+            $sql[2][] = trim($value);
+
+         }
+
+         $sqlstr = "INSERT INTO " . $this->prefix . $table ." (".implode(',',$sql[0]).") VALUES(".implode(',',$sql[1]).")";
+
+         $pre = $this->pdo->prepare($sqlstr);
+
+         $this->pdoexec($sqlstr,$sql[2]);
 
      }
 
-     public function drop($table){
+     private function array_get($table)
+     {
 
-           $this->pdoexec('DROP TABLE '.htmlspecialchars($table).'',array());
+        $tablename = [];
 
-     }
+        if(is_array($table))
+        {
 
-     public function empty_table($table){
+            foreach ($table as $value)
+            {
+                
+                $value = trim($value);
 
-           $this->pdoexec('DELETE FROM '.htmlspecialchars($table).'',array());
+                if(!empty($value))
+                {
 
-     }
+                    $tablename[] = $this->prefix . $value;
 
-     public function orderby($arr,$desasc = null){
+                }
 
-        if($desasc != null){
-
-            $this->orderby[] = $arr . ' ' . $desasc;
-            return $this;
+            }
 
         }
+        else
+        {
+            
+            $table = trim($table);
 
-        $arr = explode(',',$arr);
-        if(count($arr) > 0){
+            if(!empty($table))
+            {
 
-            foreach($arr as $ar){
+                $tablename[] = $this->prefix . $table;
 
-               $this->orderby[] = $ar;
-             
             }
 
         }
 
-        return $this;
-
-     }
-     
-     private function  where_combine_combine($select = 0){
-
-         $arr = []; $orderby = '';
-
-         $this->where_combine($this->whereand,'AND');
-
-         $this->where_combine($this->whereor,'OR');
-         
-         $limit = $this->limit_combine($this->limit);
-         
-
-         if($select = 1){
-
-            if(count($this->orderby) > 0){
-
-                $orderby = 'ORDER BY '.implode(',',$this->orderby);
-
-            }
-
-         }
-
-         if(count($this->where["var"]) > 0){
-
-             $arr = array_merge($arr,$this->where["var"]);
-
-         }
-         if(count($limit["var"]) > 0){
-
-             $arr = array_merge($arr,$limit["var"]);
-
-         } 
-         return array("sql" => $this->where["sql"].' '.$orderby.' '.$limit["sql"],"var" => $arr);
-     }
-
-     private function select_combine($where){
-
-         return 'SELECT '.implode(',',$this->selectarr).' FROM '.$this->from.'  '.(empty(trim($where)) ? '':'WHERE '.$where);
+        return implode(',',$tablename);        
 
      }
 
-     private function clearQuery(){
+     public function drop($table)
+     {
 
-        $this->selectarr = array();
-        $this->from = null;
-        $this->where = array();
-        $this->whereand = array();
-        $this->whereor = array();
-        $this->limit = array();
-        $this->orderby = array();
+        return $this->pdoexec('DROP TABLE '.$this->prefix . trim($table));
 
      }
 
-     public function result(){  // object all
+     public function empty_table($table)
+     {
 
-         $where = $this->where_combine_combine(1);
+        return $this->pdoexec('DELETE FROM '.$this->prefix . trim($table));
 
-		 $sql =  $this->select_combine($where["sql"]);
-
-        return $this->pdoexec($sql,$where["var"] , 1);
      }
-     public function result_array(){ // array all
 
-         $where = $this->where_combine_combine(1);
-         $sql =  $this->select_combine($where["sql"]);
+     public function truncate($table)
+     {
 
-        return $this->pdoexec($sql,$where["var"] , 2);
+        return $this->pdoexec('TRUNCATE '.$this->prefix . trim($table));
+
      }
-     public function get(){ // object one
 
-         $where = $this->where_combine_combine(1);
+     public function analyze($table)
+     {
 
-         $sql =  $this->select_combine($where["sql"]);
+        return $this->pdoexec('ANALYZE TABLE '.$this->prefix . trim($table),[],4);
 
-        return $this->pdoexec($sql,$where["var"] , 3);
      }
-     public function get_array(){ // array one
 
-         $where = $this->where_combine_combine(1);
+     public function checksum($table = [])
+     {
 
-         $sql =  $this->select_combine($where["sql"]);
+        $tablename = $this->array_get($table);
 
-        return $this->pdoexec($sql,$where["var"] , 4);
-     }     
+        if(empty($tablename))
+        {
 
+            return false;
+
+        }
+
+        return $this->pdoexec('CHECKSUM TABLE ' . $tablename,[],4);
+
+     }
+
+     public function optimize($table = [])
+     {
+
+        $tablename = $this->array_get($table);
+
+        if(empty($tablename))
+        {
+
+            return false;
+
+        }
+
+        return $this->pdoexec('OPTIMIZE TABLE ' . $tablename,[],4);
+
+     }
+
+     public function repair($table = [])
+     {
+
+        $tablename = $this->array_get($table);
+
+        if(empty($tablename))
+        {
+
+            return false;
+
+        }
+
+        return $this->pdoexec('REPAIR TABLE ' . $tablename,[],4);
+
+     }
+
+
+     //ANALYZE TABLE is_users
 
 }
-$db = new PDO_MYSQL;
+$db = new PDO_MYSQL([
+   'ip' => 'localhost',
+   'database' => 'is_test',
+   'dbengine' => 'mysql',
+   'username' => 'root',
+   'password' => '',
+   'charset' => 'utf8',
+   'prefix' => 'is_'
+]);
--- index.php
+++ index.php
@@ -1,11 +1,72 @@
 <?php 
 
-require "application/config/config.php";
 require "application/database/database.php";
 
+/*
+ * DROP TABLE
+ * @param $tablename
+ * return true or false
+ */
+ //$db->drop('users');
+
+/*
+ * INSERT DATA
+ * @param $tablename
+ * @param array fieldone => value ...
+ * 
+ */
+ $db->insert('users',[
+ 	'username' => 'ismail_satilmis',
+ 	'email' => 'ismaiil_0234@hotmail.com',
+ ]);
 
 
+/*
+ * EMPTY TABLE
+ * @param $tablename
+ * 
+ */
+// $db->empty_table('users');
 
 
+/*
+ * TRUNCATE TABLE
+ * @param $tablename
+ * 
+ */
+ //$db->truncate('users');
+
+ /*
+ * ANALYZE TABLE
+ * @param $tablename
+ * 
+ */
+ echo "<pre>";
+ print_r($db->analyze('users'));	
+ echo "</pre>";
+ /*
+ * CHECKSUM TABLE
+ * @param $tablename
+ * 
+ */
+ echo "<pre>";
+ print_r($db->checksum('users'));	
+ echo "</pre>";
+ /*
+ * OPTIMIZE TABLE
+ * @param $tablename
+ * 
+ */
+ echo "<pre>";
+ print_r($db->optimize('users'));	
+ echo "</pre>";
+ /*
+ * REPAIR TABLE
+ * @param $tablename
+ * 
+ */
+ echo "<pre>";
+ print_r($db->repair('users'));	
+ echo "</pre>";
 
 ?>
